075e4b2c66621726c2d591636cd93b66446480d4
Artem5105 <geraa.5105@gmail.com>
2025-11-14 19:30:18 +0300

    1

---
+++ solutions/GerasimenkoA/src/expressionanalyzer.cpp
@@ -0,0 +1,60 @@
+// ННГУ, ИИТММ, Курс "Алгоритмы и структуры данных"
+//
+// texpressionanalyzer.cpp
+//
+// Copyright (c) Пинежанин Е.С.
+
+#include <texpressionanalyzer.h>
+#include <tstack.h>
+
+#include <algorithm> 
+#include <cctype>    // isspace
+#include <vector>
+#include <string>
+
+using namespace std;
+
+void TExpressionAnalyzer::RemoveSpaces()
+{
+    string result;
+    
+    for (size_t i = 0; i < Formula.length(); i++) {
+        if (Formula[i] != ' ') {
+            result.push_back(Formula[i]);
+        }
+    }
+
+    Formula = result;
+
+}
+
+TExpressionAnalyzer::TExpressionAnalyzer(const string& _Formula) : Formula(_Formula)
+{
+    RemoveSpaces();
+    PostfixForm.clear();
+}
+
+bool TExpressionAnalyzer::FormulaChecker()
+{
+    TStack<char> st(Formula.size());
+    for (size_t i = 0; i < Formula.length(); i++) {
+        char c = Formula[i];
+        if (c == '(') st.Push(c); 
+        else if (c == ')') {
+            if (st.IsEmpty())
+                return false;
+            st.Pop();
+        }
+    }
+    return st.IsEmpty();
+}
+
+void TExpressionAnalyzer::FormulaConverter()
+{
+
+}
+
+double TExpressionAnalyzer::FormulaCalculator()
+{
+    return 0.0;
+}
